d74dfb395f5e5be65b69737eeb0bc95d529d4f69
zhang4 <ran.zhang863@hotmail.com>
2023-12-17 19:04:56 +0800

    change from pnpm to npm

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   title: "Zhangr4.github.io",
   description: "Under Construction",
   head: [
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png"}],
-    ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/favicons/site.webmanifest"}],
     ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
     [
       "link",
