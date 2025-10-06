75854472e064323edaea6f95e429b6234ed2e73f
zhangr4 <78583716+zhangr4@users.noreply.github.com>
2025-10-06 13:28:39 +0800

    Update unit test for post index generation

--- src/BlogWASM.PostIndexGenerator.Test/PostIndexGenerateTest.cs
+++ src/BlogWASM.PostIndexGenerator.Test/PostIndexGenerateTest.cs
@@ -1,18 +1,50 @@
+using Microsoft.Build.Framework;
+using Moq;
+
 namespace BlogWASM.PostIndexGenerator.Test;
 
 public class PostIndexGenerateTest
 {
+    private readonly Mock<IBuildEngine> buildEngine;
+    private readonly List<BuildMessageEventArgs> messages = [];
+    private readonly List<BuildErrorEventArgs> errors = [];
+    private readonly ITestOutputHelper testOutput;
+
+    public PostIndexGenerateTest(ITestOutputHelper testOutput)
+    {
+        this.testOutput = testOutput;
+        buildEngine = new Mock<IBuildEngine>();
+        buildEngine
+            .Setup(x => x.LogMessageEvent(It.IsAny<BuildMessageEventArgs>()))
+            .Callback<BuildMessageEventArgs>(msg => messages.Add(msg));
+        buildEngine
+            .Setup(x => x.LogErrorEvent(It.IsAny<BuildErrorEventArgs>()))
+            .Callback<BuildErrorEventArgs>(msg => errors.Add(msg));
+    }
+
     [Fact]
     public void Execute_OK()
     {
-        var generator = new PostIndexGenerate()
+        // Arrange
+        var item = new Mock<ITaskItem>();
+        var generator = new PostIndexGenerate
         {
             PostsDirectory = "Posts",
+            BuildEngine = buildEngine.Object,
         };
 
+        // Act
         var result = generator.Execute();
-        Assert.True(result);
 
-        
+        // Assert
+        foreach (var msg in messages)
+        {
+            testOutput.WriteLine($"[{msg.Importance}] {msg.Message}");
+        }
+        foreach (var err in errors)
+        {
+            testOutput.WriteLine($"[ERROR] {err.Message}");
+        }
+        Assert.True(result);
     }
 }
