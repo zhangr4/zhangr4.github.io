fe3684da7a697735548b6dd81cf14a1d91527b72
zhangr4 <78583716+zhangr4@users.noreply.github.com>
2025-10-07 12:45:02 +0800

    Add post metadata model

--- src/BlogWASM/Models/PostMetadata.cs
+++ src/BlogWASM/Models/PostMetadata.cs
@@ -2,7 +2,14 @@ using System;
 
 namespace BlogWASM.Models;
 
+/// <summary>
+/// Metadata for a blog post. Should map to yml block of origin markdown file
+/// </summary>
 public class PostMetadata
 {
-
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string[] Tags { get; set; } = [];
+    public DateTime CreatedAt { get; set; } = DateTime.MinValue;
+    public DateTime UpdatedAt { get; set; } = DateTime.MinValue;
 }
